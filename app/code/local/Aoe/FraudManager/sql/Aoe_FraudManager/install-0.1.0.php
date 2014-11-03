4128485342da66b8157fcd2ea80c57677e317fee
Lee Saferite <lee.saferite@aoe.com>
2014-11-03 19:43:03 +0000

    Adding is_fraud flag to the order and order grid tables

---
+++ app/code/local/Aoe/FraudManager/sql/Aoe_FraudManager/install-0.1.0.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @author Lee Saferite <lee.saferite@aoe.com>
+ * @since  2014-11-03
+ *
+ */
+
+$setupConfig = Mage::getConfig()->getNode('global/resources/sales_setup/setup');
+$setupClass = $setupConfig->getClassName();
+$setupClass = ($setupClass ? $setupClass : 'Mage_Sales_Model_Resource_Setup');
+$setup = new $setupClass('sales_setup');
+/* @var Mage_Sales_Model_Resource_Setup $setup */
+
+$setup->startSetup();
+
+$setup->addAttribute(
+    Mage_Sales_Model_Order::ENTITY,
+    'is_fraud',
+    array(
+        'type'     => Varien_Db_Ddl_Table::TYPE_BOOLEAN,
+        'required' => true,
+        'comment'  => 'Fraud flag',
+        'grid'     => true,
+    )
+);
+
+$setup->endSetup();
