f5baa991dc0611213cd6b9a830777559888c94ed
Lee Saferite <lee.saferite@aoe.com>
2014-11-03 20:51:10 +0000

    Adding fraud status to order view

--- app/code/local/Aoe/FraudManager/Helper/Data.php
+++ app/code/local/Aoe/FraudManager/Helper/Data.php
@@ -6,5 +6,25 @@
  */
 class Aoe_FraudManager_Helper_Data extends Mage_Core_Helper_Abstract
 {
-    // Empty
+    public function isOrderFraud($order = null)
+    {
+        $order = $this->resolveOrder($order);
+
+        if ($order instanceof Mage_Sales_Model_Order && (bool)$order->getIsFraud()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    protected function resolveOrder($order = null)
+    {
+        if ($order instanceof Mage_Sales_Model_Order){
+            return $order;
+        } elseif($order) {
+            return Mage::getModel('sales/order')->load($order);
+        } else {
+            return Mage::registry('current_order');
+        }
+    }
 }
