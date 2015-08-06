915917d00157a680731506339b92b66b80afa733
Lee Saferite <lee.saferite@aoe.com>
2015-08-06 17:30:09 +0000

    Refactor hold config/code to be store scoped

--- app/code/local/Aoe/FraudManager/Model/Observer.php
+++ app/code/local/Aoe/FraudManager/Model/Observer.php
@@ -111,15 +111,15 @@ class Aoe_FraudManager_Model_Observer
 
     public function checkQuoteSubmitSuccess(Varien_Event_Observer $observer)
     {
-        /** @var Aoe_FraudManager_Helper_HoldRule $helper */
-        $helper = Mage::helper('Aoe_FraudManager/HoldRule');
-        if (!$helper->isActive()) {
-            return;
-        }
-
         /** @var Mage_Sales_Model_Order $order */
         $order = $observer->getOrder();
         if (!$order instanceof Mage_Sales_Model_Order) {
+            return;
+        }
+
+        /** @var Aoe_FraudManager_Helper_HoldRule $helper */
+        $helper = Mage::helper('Aoe_FraudManager/HoldRule');
+        if (!$helper->isActive($order->getStoreId())) {
             return;
         }
 
@@ -164,7 +164,7 @@ class Aoe_FraudManager_Model_Observer
 
         if (count($notifications)) {
             try {
-                $helper->notify(implode("\n", $notifications), array('order' => $order));
+                $helper->notify(implode("\n", $notifications), array('order' => $order), $order->getStoreId());
             } catch (Exception $e) {
                 Mage::logException($e);
             }
